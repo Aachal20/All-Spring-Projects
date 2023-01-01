97b49dfa5d14cec71daab6cba739d65bfe4e3a9a
Aachal20 <mozareaachal999@gmail.com>
2023-01-01 13:50:07 +0530

    MSProj04-FeingClient

---
+++ MSProj04-ShoppingService-Consumer-FC/src/main/java/com/nt/controller/ShoppingController.java
@@ -0,0 +1,24 @@
+package com.nt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.nt.client.IBillingServiceRestConsumer;
+
+
+@RestController
+@RequestMapping("/shopping")
+public class ShoppingController {
+	@Autowired
+	private IBillingServiceRestConsumer consumer;
+	
+	@GetMapping("/info")
+	public  String   displayShoppingDetails() {
+		System.out.println("Shopping Controller :: Client comp class name ::" +consumer.getClass());
+		return  "Pongal Shopping for Family ...."+consumer.fetchBillDetails();
+		
+	}
+	
+
+}
