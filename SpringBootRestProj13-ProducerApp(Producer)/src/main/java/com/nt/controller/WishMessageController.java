4d56548985411d4f41f1cf7822600b5b5e88f4d2
Aachal20 <mozareaachal999@gmail.com>
2023-01-04 11:20:18 +0530

    SB-Rest13-ProducerApp(Producer)

---
+++ SpringBootRestProj13-ProducerApp(Producer)/src/main/java/com/nt/controller/WishMessageController.java
@@ -0,0 +1,18 @@
+package com.nt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wish/api")
+
+public class WishMessageController {
+
+	@GetMapping("/message")
+	public ResponseEntity<String> showWishMEssage(){
+		return new ResponseEntity<String>("Good Afternonn" ,HttpStatus.OK);
+	}
+}
