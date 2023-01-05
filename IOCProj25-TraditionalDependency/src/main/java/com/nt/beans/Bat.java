cbd01f6affd6edb89aac24a7ebcc51bc5914b93e
Aachal20 <mozareaachal999@gmail.com>
2023-01-05 13:46:35 +0530

    IOCProj25-TraditionalDependency

---
+++ IOCProj25-TraditionalDependency/src/main/java/com/nt/beans/Bat.java
@@ -0,0 +1,14 @@
+package com.nt.beans;
+
+import java.util.Random;
+
+public class Bat {
+
+	public int scoreRun() {
+		System.out.println("Bat.scoreRun()");
+		return new Random().nextInt(100);
+	}
+	public Bat() {
+		System.out.println("Bat.Bat()::0-param constructor");
+	}
+}
