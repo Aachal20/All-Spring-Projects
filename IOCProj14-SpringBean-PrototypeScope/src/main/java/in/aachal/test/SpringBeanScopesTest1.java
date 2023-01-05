f4fd3a0cd740c1e9468cde3e082a55d05aa8f69e
Aachal20 <mozareaachal999@gmail.com>
2023-01-05 11:15:09 +0530

    IOCProj14-SpringBean-PrototypeScope

---
+++ IOCProj14-SpringBean-PrototypeScope/src/main/java/in/aachal/test/SpringBeanScopesTest1.java
@@ -0,0 +1,32 @@
+package in.aachal.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import in.aachal.beans.WishMessageGenerator;
+
+public class SpringBeanScopesTest1 {
+
+	public static void main(String[] args) {
+		
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("in/aachal/cfgs/applicationContext.xml");
+		
+		//get raget class object
+		WishMessageGenerator generator = factory.getBean("wmg",WishMessageGenerator.class);
+		//WishMessageGenerator generator1 = factory.getBean("wmg",WishMessageGenerator.class);
+		WishMessageGenerator generator1 = factory.getBean("wmg1",WishMessageGenerator.class);
+		
+		System.out.println("=============================");
+		System.out.println("generator1==geneartor  ::"  +(generator==generator1));
+		System.out.println("gen1 Hashhocdoe :: "+generator.hashCode()+ " gen2 Hashcode ::" +generator1.hashCode());
+		//invoke the BM
+		//String msg = generator.generateMessage("Aachal");
+		//System.out.println("Wish MEssage is :: "+msg);
+	}
+
+}
