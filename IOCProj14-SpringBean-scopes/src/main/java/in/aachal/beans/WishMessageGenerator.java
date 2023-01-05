0f690146e8d3b54d2de3f05fc6581ad1fefc8156
Aachal20 <mozareaachal999@gmail.com>
2023-01-05 11:10:26 +0530

    IOCProj14-SpringBean-Singletonescope

---
+++ IOCProj14-SpringBean-scopes/src/main/java/in/aachal/beans/WishMessageGenerator.java
@@ -0,0 +1,35 @@
+package in.aachal.beans;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class WishMessageGenerator {
+	//HAS-A property (supporting composition)
+	private Date date;
+
+
+	public WishMessageGenerator(Date date) {
+		this.date=date;
+		System.out.println("WishMessageGenerator.WishMessageGenerator()::0-parm constru");
+		System.out.println("system date and time::"+date);
+	}
+
+	public String generateMessage(String msg) {
+		System.out.println("WishMessageGenerator.generateMessage()" +date);
+		
+		int hour = date.getHours();
+		if(hour<12)
+			return "GM"+msg;
+		else if(hour<22)
+			return "GA" +msg;
+		else if(hour<27)
+			return "GE"+msg;
+		else
+			return "GN"+msg;
+
+	}
+
+
+}
